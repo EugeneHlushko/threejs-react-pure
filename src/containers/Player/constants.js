1393a4ff78303dca1cdc42d0ae3c7d97e8417f42
EugeneHlushko <jhlushko@gmail.com>
2017-01-31 10:27:42 +0200

    fix player constants

--- src/containers/Player/constants.js
+++ src/containers/Player/constants.js
@@ -1,13 +1,13 @@
-const PLAYER_IS_WALKING = 'App/Player/constants/PLAYER_IS_WALKING';
-const PLAYER_IS_STANDING = 'App/Player/constants/PLAYER_IS_STANDING';
+export const PLAYER_IS_WALKING = 'App/Player/constants/PLAYER_IS_WALKING';
+export const PLAYER_IS_STANDING = 'App/Player/constants/PLAYER_IS_STANDING';
 
 // setters
-const PLAYER_SET_MOTION = 'App/Player/constants/PLAYER_SET_MOTION';
+export const PLAYER_SET_MOTION = 'App/Player/constants/PLAYER_SET_MOTION';
 
 // modification actions
-const PLAYER_MODIFY_HEALTH = 'App/Player/constants/PLAYER_MODIFY_HEALTH';
-const PLAYER_MODIFY_SPEED = 'App/Player/constants/PLAYER_MODIFY_SPEED';
+export const PLAYER_MODIFY_HEALTH = 'App/Player/constants/PLAYER_MODIFY_HEALTH';
+export const PLAYER_MODIFY_SPEED = 'App/Player/constants/PLAYER_MODIFY_SPEED';
 
 // resetting actions
-const PLAYER_RESET_HEALTH = 'App/Player/constants/PLAYER_RESET_HEALTH';
-const PLAYER_RESET_SPEED = 'App/Player/constants/PLAYER_RESET_SPEED';
+export const PLAYER_RESET_HEALTH = 'App/Player/constants/PLAYER_RESET_HEALTH';
+export const PLAYER_RESET_SPEED = 'App/Player/constants/PLAYER_RESET_SPEED';
